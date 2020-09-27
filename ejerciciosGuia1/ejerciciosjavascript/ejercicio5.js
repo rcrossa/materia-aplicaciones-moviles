6d801a536115c424189788def6bc20b06a3c9b1f
Roberto Rossa <rcrossa@hotmail.com>
2020-09-26 23:47:01 -0300

    update
    ejercicio 7

--- ejerciciosGuia1/ejerciciosjavascript/ejercicio5.js
+++ ejerciciosGuia1/ejerciciosjavascript/ejercicio5.js
@@ -4,7 +4,7 @@ let notas = [8,6,2,4];
 // notas.forEach(Element => Element > 4({ 
 //     notas = notas.map
 // }));
-var aprobaron = notas.map(function(element){
+var aprobaron = notas.filter(function(element){
     
     return console.log(element > 4);
 })
--- ejerciciosGuia1/ejerciciosjavascript/ejercicio6.js
+++ ejerciciosGuia1/ejerciciosjavascript/ejercicio6.js
@@ -1,8 +1,12 @@
-const notas = [[8,6,2,4],[7,9,4,5],[6,2,4,2],[9,6,7,10]];
+const quienesAprobaron = [[8,6,2,4],[7,9,4,5],[6,2,4,2],[9,6,7,10]];
 
-  const quienesAprobaron = notas.map(Element => Element.nota > 4 );
-
-  console.log(quienesAprobaron);
+  // const quienesAprobaron = notas.map(Element =>  );
+  quienesAprobaron.forEach(element => {
+  
+      console.log(element > 4);
+   
+  });
+  // console.log(quienesAprobaron);
   //   let nota =  [8,6,2,4,];
 // console.log(notas);
 // var aprobaron = notas.map(function(element){
