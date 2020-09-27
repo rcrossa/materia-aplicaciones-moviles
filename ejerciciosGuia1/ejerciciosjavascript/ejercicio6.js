37b1adcbd7d66b376d3e8c6b4a1e4c7067ff466b
Roberto Rossa <rcrossa@hotmail.com>
2020-09-26 23:02:12 -0300

    Update ejercicio6.js
    correccion

--- ejerciciosGuia1/ejerciciosjavascript/ejercicio6.js
+++ ejerciciosGuia1/ejerciciosjavascript/ejercicio6.js
@@ -1,6 +1,6 @@
 const notas = [[8,6,2,4],[7,9,4,5],[6,2,4,2],[9,6,7,10]];
 
-  const quienesAprobaron = notas.filter(Element => Element.nota > 4 );
+  const quienesAprobaron = notas.map(Element => Element.nota > 4 );
 
   console.log(quienesAprobaron);
   //   let nota =  [8,6,2,4,];
