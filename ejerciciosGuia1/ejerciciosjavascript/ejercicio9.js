68e9a23a140ec5e4f4f1ea9719651f9a1330e721
Roberto Rossa <rcrossa@hotmail.com>
2020-09-27 00:03:25 -0300

    Update ejercicio9.js
    correccion

--- ejerciciosGuia1/ejerciciosjavascript/ejercicio9.js
+++ ejerciciosGuia1/ejerciciosjavascript/ejercicio9.js
@@ -1,2 +1,10 @@
-let numeros = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17];
-let a = numeros.map(num => {if (num%2==1){ console.log(num)}});
+const numeros = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17];
+const numeros1=[]
+
+const rechazar = numeros.map(function(num){
+    if (num%2==0) {
+        numeros1.push(num);
+    }
+    
+})
+console.log(numeros1);
