15d879a407c5a57fcf5ce9dd0628ac1e7c3cdcc2
Roberto Rossa <rcrossa@hotmail.com>
2020-10-04 16:53:47 -0300

    update ejercicio 3

--- EjercicioGuia2/ejerciciosJSAsync-master/ejercicio_3/app.js
+++ EjercicioGuia2/ejerciciosJSAsync-master/ejercicio_3/app.js
@@ -3,13 +3,17 @@ const obtenerPokemon = require("./library");
 
 // Codigo funcion callback
 
-c
-
-
+nombrePokemon = "kakuna";
+function detalle1(detalle){
+    // console.log(nombre);
+    return console.log(detalle);
+}
+// nombrePokemon= null;
 // Fin codigo
 
 // Usar la funcion obtenerPokemon(pokemon,funcionCallback) en donde:
 // - nombrePokemon es un string con el nombre del pokemon
 // - funcionCallback es una función que recibe el objeto con los datos del pokemon extraido
 
-obtenerPokemon();
+obtenerPokemon(nombrePokemon,detalle1);
+
--- EjercicioGuia2/ejerciciosJSAsync-master/ejercicio_4/app.js
+++ EjercicioGuia2/ejerciciosJSAsync-master/ejercicio_4/app.js
@@ -1,5 +1,27 @@
 
 const obtenerPokemon = require("./library");
+nombrePokemon="kakuna";
+function obtenerChiste(nombrePokemon) {
+    return console.log(nombrePokemon);
+}
+obtenerChiste().then(function(primerElemento) {
+    return console.log(primerElemento);
+}).then(function(segundoElemento) {
+    return console.log(segundoElemento);
+}).then(function(tercerElemento){
+    return console.log(tercerElemento);
+}).catch(function(err) {
+   console.error(); 
+});
 
+// obtenerChiste().then(function(primerElemento){
+//     return console.log(primerElemento[0].id);
+// }).then(function(segundoElemento){
+//     return segundoElemento[0].setup;
+// }).then(function(tercerElemento){
+//     return tercerElemento[0].punchline;
+// }).catch(function(err){
+//     console.error();
+// });
 // Usar la funcion obtenerChiste(nombrePokemon) la cual recibe como string un nombre de pokemon y devuelve la promesa de traer el objeto pokemon extraido
-obtenerPokemon();
+obtenerPokemon(obtenerChiste);
