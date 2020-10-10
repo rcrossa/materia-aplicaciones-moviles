38723cb341d4b1385c14681d43601e49010f77a7
Roberto Rossa <rcrossa@hotmail.com>
2020-10-10 20:10:37 -0300

    Se realiza ejercicio de api
    creaccion de ejercicio api

---
+++ api/my-app/src/App.js
@@ -0,0 +1,19 @@
+import React,{Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Mostrar from './componentes/muestraap';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <Mostrar/>
+      </header>
+      
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ api/my-app/src/componentes/muestraap.js
@@ -0,0 +1,36 @@
+import React, { useCallback } from 'react';
+
+class Mostrar extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            detalle: [],
+            isFetch: true,
+        }
+    }
+    componentDidMount(){
+        fetch('https://restcountries.eu/rest/v2/name/argentina')
+        .then(response => response.json())
+        .then(detalleJson => this.setState({detalle: detalleJson[0], isFetch: false}))
+    }
+    render(){
+        if(this.state.isFetch){
+            return 'Loading..'
+        }else{
+            
+        const dato1=this.state.detalle.name
+        const dato2=this.state.detalle.capital
+        const dato3=this.state.detalle.region
+        const dato4=this.state.detalle.timezones
+     
+        return <div>
+            <h1>{dato1}</h1>
+            <h2>{dato2}</h2>
+            <h3>{dato3}</h3>
+            <h4>{dato4}</h4>
+        </div>
+        }
+    }
+}
+
+export default Mostrar;
