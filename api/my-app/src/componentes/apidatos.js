75858ee88f39f7dff4a0779a0a86e835a907631c
Roberto Rossa <rcrossa@hotmail.com>
2020-11-10 21:02:52 -0300

    Update apidatos.js

--- api/my-app/src/componentes/apidatos.js
+++ api/my-app/src/componentes/apidatos.js
@@ -6,71 +6,71 @@ class MostrarApi extends React.Component {
     super(props);
     this.state = {
       value: 'Please write an essay about your favorite DOM element.',
-      value1:'',
       name: "",
       detalle: [],
-      numero:'',
       pais:'Argentina',
-      url:'https://restcountries.eu/rest/v2/name/',
+      url:'https://restcountries.eu/rest/v2/name/argentina',
       loading: false
     };
   }
     
   componentDidMount(){
-    fetch('url'+'pais')
+    fetch('https://restcountries.eu/rest/v2/name/argentina')
     .then(response => response.json())
     .then(detalleJson => this.setState({detalle: detalleJson[0], isFetch: false}))
 }
 
-// getDetalle =  (numero) => {
-//   this.setState({loading:true})
-//   fetch(this.state.url)
-//   .then(response => response.json())
-//   .then(detalleJson =>this.setState({detalle: detalleJson[0]}))
-//   .catch(console.log('algo salio mal'));
 
-// }
-
-  handleChange = event => {
+  // handleChange = event => {
+  //   const value = event.target.value;
+  //   this.setState({
+  //     name: value
+  //   });
+  // }
+    handleChange = event => {
     const value = event.target.value;
+    const dato = this.state.detalle.name;
+    const dato5 = this.state.detalle.timezones;
+    const dato1 = this.state.detalle.capital;
+    const dato2 = this.state.detalle.region;
+    const dato3 = this.state.detalle.borders;
+    const dato4 = this.state.detalle.subregion;
+    // for (let index = 0; index < dato.length; index++) {
+    //   if(dato == dato5){
+    //     break;
+    //   }
+    //   const element = dato[index];
+      
+    // }
+    // const dato = datos.filter(Element => Element =='argentina');
+    if(this.state.detalle.name == value){
     this.setState({
-      name: value
-    });
-  }
-  handleChange1 = event => {
-  const value1 = event.target.value1;
-    this.setState({
-      name1: value1
-    });
-  }
+      name: " Pais: " + dato 
+      + " Capital: " +  dato1 
+      + " Region: " + dato2 
+      + " Fronteras: " + dato3 
+      + " Subregion: " + dato4 
+      + " Time Zones: " + dato5 });
+      
+    }else{
+      this.setState({
+         name: 'pais no encontrado'
+      });
+     
+    }
+}
 
   handleReverse = event => {
     event.preventDefault();
     const text  = this.state.name;
     this.setState({
       reversedText:text
-      .split("")
-      .reverse()
-      .join("")
     })
   }
-  handleReverse1 = event => {
-    event.preventDefault();
-    const text  = this.state.name;
-    this.setState({
-      reversedText1:text
-      .split("")
-      .reverse()
-      .join("")
-    })
-  }
+   
+
  
   render() {
-    const date = this.state.detalle;
-    const dato = (value1) =>{
-      date.filter(this.state.detalle.name);
-    }
-    const date1 = this.state.detalle.argentina;
   
     return (
       <React.Fragment>
@@ -93,28 +93,9 @@ class MostrarApi extends React.Component {
             <button>Reverse Text</button>
           </div>
         </form>
-        <form onSubmit={this.handleReverse1}>
-          <div>
-            { /* Render input entered */}
-            <label>Text1: {this.state.input1}</label>
-          </div>
-          <div>
-           { /* handleChange() is triggered when text is entered */ }
-            <input
-              type="text"
-              value={this.state.input1}
-              onChange={this.handleChange.value1}
-              placeholder="Enter a text1"
-            />
-          </div>
-          <div>
-            <button>Reverse Text</button>
-          </div>
-        </form>
+      
         { /* Render reversed text */}
         <p>Reversed Text: {this.state.reversedText}</p>
-        <p>Reversed Text: {}</p>
-
       </React.Fragment>
     );
   }
