f779cb4c5a5b8a9943f1e0b7e50786e09c7296f6
Roberto Rossa <rcrossa@hotmail.com>
2020-11-24 00:08:05 -0300

    update

--- TerceraEntrega/bp-StackNavigator-RN-main/Screens/firebase.js
+++ TerceraEntrega/bp-StackNavigator-RN-main/Screens/firebase.js
@@ -1,9 +1,9 @@
 export default {
-    apiKey: "",
-    authDomain: "auth-4531d.firebaseapp.com",
-    databaseURL: "https://auth-4531d.firebaseio.com",
-    projectId: "auth-4531d",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
+    apiKey: "AIzaSyA3H58Zsx1dA74wZQ6xuhXNkmK9YF_qfBc",
+  authDomain: "auth-4531d.firebaseapp.com",
+  databaseURL: "https://auth-4531d.firebaseio.com",
+  projectId: "auth-4531d",
+  storageBucket: "auth-4531d.appspot.com",
+  messagingSenderId: "977942455082",
+  appId: "1:977942455082:web:d3eb098fbc0ca2114ff453"
 }
